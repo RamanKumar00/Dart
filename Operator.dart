044c64bb14661ff017a8e6b347a67822bca670aa
RamanKumar00 <ramankr7321@gmail.com>
2024-11-07 21:18:19 +0530

    my first commit for dart

---
+++ Operator.dart
@@ -0,0 +1,40 @@
+void main() {
+  int a = 5;
+  int b = 8;
+
+  //addition operator
+
+  var c = a + b;
+  print("sum (a+b)=$c");
+
+  //subtract operator
+
+  var d = b - a;
+  print("sub (b-a)=$d");
+
+  //multiplication operator
+
+  var e = a * b;
+  print("multiply (a*b)=$e");
+
+  //division operator
+
+  var f = a / b;
+  print("division (a/b)=$f");
+
+  //modulus operator
+
+  var g = a % b;
+  print("modulus (a%b)=$g");
+
+  var h = a ~/ b;
+  print("quotient (a~/b)=$h");
+
+  //Relational operator
+
+  var i = a > b;
+  print("a is greater than b (a > b) : $i");
+
+  var j = a < b;
+  print("b is less than b (a < b): $j");
+}
